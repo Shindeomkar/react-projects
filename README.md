492de1ec0253012d1e0a256a1c62d17e477e7f17
Shindeomkar <omkar3631@gmail.com>
2021-06-07 10:07:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-a2wdnd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-a2wdnd)
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>this ia first react projrct on stack blitz</h1>
+    </div>
+  );
+}
